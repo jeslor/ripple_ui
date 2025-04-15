9dfff22e046986a7d603321ca8af296a80839aa0
Jeslor Ssozi <jbestius@gmail.com>
2025-04-15 11:27:13 +0200

    reconfigure vite to use tailwind css

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,36 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 import "./App.css";
 
+const circles = Array.from({ length: 8 });
+
 function App() {
   const [count, setCount] = useState(0);
+  const [isReady, setIsReady] = useState(false);
+
+  console.log(circles);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setIsReady(true);
+    }, 1000);
+
+    return () => clearTimeout(timer);
+  }, []);
 
   return (
-    <main className="h-screen w-screen bg-slate-900">
-      <h1 className="text-slate-300">Hello</h1>
+    <main className="h-screen w-screen bg-slate-900 flex justify-center items-center">
       <div className="relative flex justify-center items-center ">
-        <div className="bg-slate-700 absolute h-[20px] w-[20px]"></div>
-        <div className="bg-slate-700 absolute h-[20px] w-[20px]"></div>
-        <div className="bg-slate-700 absolute h-[20px] w-[20px]"></div>
-        <div className="bg-slate-700 absolute h-[20px] w-[20px]"></div>
-        <div className="bg-slate-700 absolute h-[20px] w-[20px]"></div>
-        <div className="bg-slate-700 absolute h-[20px] w-[20px]"></div>
-        <div className="bg-slate-700 absolute h-[20px] w-[20px]"></div>
-        <div className="bg-slate-700 absolute h-[20px] w-[20px]"></div>
+        {isReady &&
+          circles.map((_, index) => (
+            <div
+              style={{ width: `${(index + 1) * 100}px`, height: "100px" }}
+              key={index}
+              className={` rounded-full bg-slate-200 absolute transition-all duration-1000 ease-in-out ${
+                count === index ? "scale-100" : "scale-0"
+              }`}
+            ></div>
+          ))}
       </div>
     </main>
   );
