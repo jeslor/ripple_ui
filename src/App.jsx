4a14a0e1da04c5c88812fef93005bf68af55ca10
Jeslor Ssozi <jbestius@gmail.com>
2025-04-17 12:33:45 +0200

    perfect the colors

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,7 @@ function App() {
     }
   `;
   return (
-    <main className="h-screen w-screen bg-slate-900 flex justify-center items-center">
+    <main className="h-screen w-screen bg-black flex justify-center items-center">
       <div className="relative w-[600px] h-[600px] mx-auto my-20 flex items-center justify-center">
         <div className="absolute top-1/2 left-1/2 w-6 h-6 bg-gray-400 rounded-full -translate-x-1/2 -translate-y-1/2 z-50 flex items-center justify-center text-white">
           +
@@ -26,17 +26,17 @@ function App() {
         {[...Array(8)].map((_, i) => (
           <div
             key={i}
-            className={`absolute  rounded-full border border-gray-700/40  `}
+            className={`absolute  rounded-full border-[1px]  transition-all duration-1000 ease-in-out`}
             style={{
-              width: `${120 + i * 80}px`,
-              height: `${120 + i * 80}px`,
-              // transform: "translate(-50%, -50%)",
+              width: `${200 + i * 80}px`,
+              height: `${200 + i * 80}px`,
+              borderColor: `rgba(89, 89, 89, 0.${Math.abs(i + 1 - 9)})`,
               borderRadius: "50%",
               transformOrigin: "50% 50%",
-              animation: `scale ${i + 1 * 2}s linear infinite alternate`,
-              animationDelay: `${i + 2}s`,
+              animation: `scale ${2}s linear infinite alternate`,
               // boxShadow: `0 0 0 2px rgba(255, 255, 255, 0.1), 0 0 0 4px rgba(255, 255, 255, 0.1), 0 0 0 6px rgba(255, 255, 255, 0.1), 0 0 0 8px rgba(255, 255, 255, 0.1), 0 0 0 10px rgba(255, 255, 255, 0.1)`,
-              background: `radial-gradient(circle at 50% 50%, rgba(255, 255, 255, 0.1) 0%, rgba(255, 255, 255, 0.1) 20%, rgba(255, 255, 255, 0.1) 40%, rgba(255, 255, 255, 0.1) 60%, rgba(255, 255, 255, 0.1) 80%, rgba(255, 255, 255, 0.1) 100%)`,
+              background: `rgba(70, 70, 70, 0.${Math.abs(i + 1 - 8)})`,
+              zIndex: `${Math.abs(i - 8)}`,
             }}
           />
         ))}
@@ -63,7 +63,7 @@ function App() {
           return (
             <div
               key={i}
-              className="absolute w-10 h-10 rounded-full bg-white shadow-lg flex items-center justify-center "
+              className="absolute z-[10] w-10 h-10 rounded-full bg-white shadow-lg flex items-center justify-center "
               style={{
                 animation: `scale 1s linear infinite alternate`,
                 left: `calc(50% + ${x}px - 20px)`,
